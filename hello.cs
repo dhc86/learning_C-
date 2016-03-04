b11962174cabbc81523dff0a25c1f3f7bf10587d
DIEGO <diegoh_c@yahoo.com>
2016-03-04 14:36:36 -0800

    math operations

--- hello.cs
+++ hello.cs
@@ -10,5 +10,20 @@ public class HelloWorld
         Console.Write ("Please enter your age :");
         string age = Console.ReadLine();
         Console.WriteLine ( name + "," + " you are " + age +" years old!");
+        var somevartiable = "This is a string variable";
+        Console.WriteLine (somevartiable);
+        Console.WriteLine ("somevartiable is a {0}", somevartiable.GetTypeCode());
+        Console.WriteLine("(5 + 5 ) = " + (5 + 5));
+        Console.WriteLine("(5 - 5 ) = " + (5 - 5));
+        Console.WriteLine("(5 % 2) = " + (5 % 2));
+
+        int i = 0;
+
+        Console.WriteLine(" i++ = " + (i++));
+        Console.WriteLine("new value for i = " + i);
+
+        i = 10;
+
+        Console.WriteLine("i += 3 = " , (i += 3));
     }
 }
