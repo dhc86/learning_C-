2dfadac64895dbea68e0ac170b528219e2ec534d
DIEGO <diegoh_c@yahoo.com>
2016-03-04 15:17:36 -0800

    conditional operations

--- hello.cs
+++ hello.cs
@@ -33,19 +33,52 @@ public class HelloWorld
 
     // Console.WriteLine("i += 3 = " , (i += 3));
 
-    //casting 
-    double pi = 3.1415;
-    Console.WriteLine("pi = " + pi);
-    int intPi = (int)pi;
-    Console.WriteLine("intPi = " + intPi);
+    // //casting 
+    // double pi = 3.1415;
+    // Console.WriteLine("pi = " + pi);
+    // int intPi = (int)pi;
+    // Console.WriteLine("intPi = " + intPi);
 
-    // math functions
-    Console.WriteLine("Math.Sqrt(intPi) = " +  (Math.Sqrt(intPi)));
-    Console.WriteLine("Math.Round(pi) = " +  (Math.Round(pi)));
+    // // math functions
+    // Console.WriteLine("Math.Sqrt(intPi) = " +  (Math.Sqrt(intPi)));
+    // Console.WriteLine("Math.Round(pi) = " +  (Math.Round(pi)));
 
-    //generate random numbers
-    Random rand = new Random();
-    Console.WriteLine("random number between 1 to 10 = " + rand.Next(1,11)); 
+    // //generate random numbers
+    // Random rand = new Random();
+    // Console.WriteLine("random number between 1 to 10 = " + rand.Next(1,11)); 
+
+    //conditions 
+
+    Console.WriteLine("Please enter your age again! : ");
+    var age_string = Console.ReadLine();
+    int age = Int32.Parse(age_string);
+    if (age < 30){
+      Console.WriteLine("Good age!, age = " + age);
+    }
+    else if (age == 30){
+      Console.WriteLine("This is your actual age!");
+    }
+    else{
+      Console.WriteLine("What is wrong with you!");
+    }
+
+
+    Console.WriteLine("What is todys day of the week?");
+    var day = Console.ReadLine().ToLower();
+    switch (day){
+      case "monday":
+        Console.WriteLine("Monday time to go to work!");
+        break;
+
+      case "friday":
+        Console.WriteLine("Friday time to party!");
+        break;
+
+      default:
+        Console.WriteLine("plan your weekend nowwwwww.");
+        break;
+    }
+
 
 
 
